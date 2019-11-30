e5001ee273cf66fd9a015ffde01af2aa901068fd
bradley.l <bradley.l@kakaocrop.com>
2019-12-01 00:25:58 +0900

    프로젝트 초기화

--- src/Main.kt
+++ src/Main.kt
@@ -1,7 +1,3 @@
 fun main() {
-    println("kakaohairshop-1-test(2)")
-    println("kakaohairshop-1-test(1)")
-    println("kakaohairshop-1-(3)")
-    println("kakaohairshop-1-(2)")
-    println("kakaohairshop-1-(1)")
+    
 }
